782a21dbde60c10fc06ec8160e65c9595081a54e
Alexey Konischev <50020189+Cubaweeroi@users.noreply.github.com>
2024-04-14 19:38:48 +0300

    Update ModelsClasses.py

--- models/ModelsClasses.py
+++ models/ModelsClasses.py
@@ -2,52 +2,54 @@ from torch import nn
 import torch.nn.functional as F
 from models.ModelsUtils import Model
 
-
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
+class DepthwiseSeparableConv(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(DepthwiseSeparableConv, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=stride, padding=1, groups=in_channels)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
-        return self.double_conv(x)
-
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
 
 class Covid(Model):
-    @property
-    def y_field_name(self) -> str:
-        return "labels"
-
-    @property
-    def x_field_name(self) -> str:
-        return "original_image"
-
     def __init__(self):
-        super().__init__()
-        self.model = nn.Sequential(
-            DoubleConv(1, 64),
-            nn.MaxPool2d(2),
-            DoubleConv(64, 64),
-            nn.MaxPool2d(2),
-            DoubleConv(64, 128),
-            nn.MaxPool2d(2),
-            DoubleConv(128, 256),
-            nn.MaxPool2d(2),
-            DoubleConv(256, 256),
-            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
-            DoubleConv(128, 128),
-            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
-            DoubleConv(64, 64),
-            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
-            DoubleConv(32, 32),
-            nn.Flatten(),
-            nn.Linear(16 * 128 * 256, 3)
+        super(Covid, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            DepthwiseSeparableConv(32, 64, stride=1),
+            nn.ReLU(inplace=True),
+            DepthwiseSeparableConv(64, 128, stride=2),
+            nn.ReLU(inplace=True),
+            DepthwiseSeparableConv(128, 128, stride=1),
+            nn.ReLU(inplace=True),
+            DepthwiseSeparableConv(128, 256, stride=2),
+            nn.ReLU(inplace=True),
+            DepthwiseSeparableConv(256, 256, stride=1),
+            nn.ReLU(inplace=True),
+            DepthwiseSeparableConv(256, 512, stride=2),
+            nn.ReLU(inplace=True),
+            *[
+                nn.Sequential(
+                    DepthwiseSeparableConv(512, 512, stride=1),
+                    nn.ReLU(inplace=True)
+                ) for _ in range(5)
+            ],
+            DepthwiseSeparableConv(512, 1024, stride=2),
+            nn.ReLU(inplace=True),
+            DepthwiseSeparableConv(1024, 1024, stride=1),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(1024, 3)
         )
 
     def forward(self, x):
-        x = self.model(x)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
         return F.log_softmax(x, dim=1)
